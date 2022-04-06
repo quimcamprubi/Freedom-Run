2550262137d4d5e87db019339e69dfda41929c5b
Ferran <ferranmreyes@gmail.com>
2022-04-06 11:17:43 +0200

    Wall Jump, Bug Wall Climbing fixed

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -110,8 +110,9 @@ public class PlayerController : MonoBehaviour
     }
 
     private void WallJump() {
-        if (_isWallSliding && Mathf.Sign(-_input) != Mathf.Sign(_previousWallJumpDirection) || Mathf.Approximately(_previousWallJumpDirection,0.0f)) {
-            _previousWallJumpDirection = _input;
+        bool canWallJump = Mathf.Sign(_rigidbody2D.transform.localScale.x) != Mathf.Sign(_previousWallJumpDirection);
+        if (canWallJump && _isWallSliding || Mathf.Approximately(_previousWallJumpDirection,0.0f)) {
+            _previousWallJumpDirection = _rigidbody2D.transform.localScale.x;
             _isWallSliding = false;
             _isWallJumping = true;
             _newForce.Set(-xWallForce * transform.localScale.x, yWallForce);
@@ -220,6 +221,7 @@ public class PlayerController : MonoBehaviour
     private void ApplyMovement() {
         if (_input < 0.0f) {
             transform.localScale = new Vector2(-1.0f, 1.0f);
+            
         } else if (_input > 0.0f) {
             transform.localScale = new Vector2(1.0f, 1.0f);
         }
