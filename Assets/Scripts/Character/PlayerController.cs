3847c52a58dcbac4f3f3acb76b9d47ecc669260e
Quim Camprubí Casas <quimcc08@gmail.com>
2022-04-15 14:27:04 +0200

    Implemented collectible system: - You can grab collectible items pressing the 'E' key. - Collectible Item is an abstract class with 2 subtypes: KeyItem and RegularItem - KeyItem is used to unlock doors - Implemented DoorController class. Doors can be locked or unlocked. If they are unlocked, they can be opened without a key. If they are locked, they require a KeyItem with the matching key ID. This ID can be entered in the Unity Editor. For this system to work, both the Door and the Key must have the same ID. Othersiwe, the door will stay locked. - Implemented new sprite layers for correct sorting of the sprites. - Improved level 1 design. Added movable box, damage spikes zone. Added collectible items

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using CollectibleItems;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -21,10 +24,12 @@ public class PlayerController : MonoBehaviour
     public float wallSlidingSpeed;
     public float xWallForce;
     public float yWallForce;
-
-    
     public float jumpTime;
     public float coyoteTime;
+    
+    [HideInInspector]
+    public List<KeyItem> keysList;
+    public List<RegularItem> itemsList;
 
     // Private attributes
     private bool _isSprinting = false;
@@ -57,6 +62,15 @@ public class PlayerController : MonoBehaviour
     private float _modifiedJumpSpeed;
     private float _coyoteTimeCounter;
 
+    // Collectible items
+    private CollectibleItem availableCollectibleItem = null;
+    private bool canAddCollectible = false;
+    private GameObject objectToDestroy = null;
+    
+    // Doors
+    private DoorController availableDoor = null;
+    private bool canOpenDoor = false;
+
     void Start() {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _capsuleCollider = GetComponent<CapsuleCollider2D>();
@@ -67,7 +81,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update() {
         CheckInput();
-        
     }
 
     private void FixedUpdate() {
@@ -78,6 +91,23 @@ public class PlayerController : MonoBehaviour
     }
 
     private void CheckInput() {
+        if (Input.GetKey(KeyCode.E)) {
+            if (canAddCollectible) {
+                AddCollectible();
+                objectToDestroy.SetActive(false);
+            } else if (canOpenDoor) {
+                if (availableDoor.isLocked) { // If door is locked, check if player has the necessary key
+                    if (keysList.Any(key => key.collectibleItemId == availableDoor.unlockKeyId)) {
+                        availableDoor.OpenDoor();
+                    } else {
+                        Debug.Log("Locked"); // TODO: In the future, change this for UI message. 
+                    }
+                } else { // If door is unlocked, open it
+                    availableDoor.OpenDoor();
+                }
+            }
+        }
+        
         if (Input.GetButtonUp("Jump"))
         {
             _isJumping = false;
@@ -110,9 +140,20 @@ public class PlayerController : MonoBehaviour
         if (_isGrounded && !_isJumping) { Animator.SetBool("isGrounded", true); }
         else { Animator.SetBool("isGrounded", false); }
 
-        if (_isJumping == false) { Animator.SetBool("isJumping", false); }
-        if (_isJumping == true) { Animator.SetBool("isJumping", true); }
+        if (!_isJumping) { Animator.SetBool("isJumping", false); }
+        else { Animator.SetBool("isJumping", true); }
 
+    }
+
+    private void AddCollectible() {
+        switch (availableCollectibleItem) {
+            case KeyItem key:
+                keysList.Add(key);
+                break;
+            case RegularItem item:
+                itemsList.Add(item);
+                break;
+        }
     }
 
     private void WallJump() {
@@ -239,7 +280,7 @@ public class PlayerController : MonoBehaviour
             _newVelocity.Set(_rigidbody2D.velocity.x, Mathf.Clamp(_rigidbody2D.velocity.y, -wallSlidingSpeed, float.MaxValue));
             _rigidbody2D.velocity = _newVelocity;
         }
-        else if (ajupirse == true)
+        else if (ajupirse)
         {
             _newVelocity.Set(ajupidSpeed * _input * _sprintModifier, _rigidbody2D.velocity.y);
             _rigidbody2D.velocity = _newVelocity;
@@ -261,4 +302,27 @@ public class PlayerController : MonoBehaviour
             _rigidbody2D.velocity = _newVelocity;
         }
     }
+
+    public void AvailableCollectibleItem(CollectibleItem item, GameObject keyObject) {
+        availableCollectibleItem = item;
+        objectToDestroy = keyObject;
+        canAddCollectible = true;
+    }
+
+    public void NoAvailableCollectibleItem() {
+        availableCollectibleItem = null;
+        objectToDestroy = null;
+        canAddCollectible = false;
+    }
+
+    public void AvailableDoor(DoorController doorController) {
+        availableDoor = doorController;
+        canOpenDoor = true;
+    }
+
+    public void NoAvailableDoor() {
+        availableDoor = null;
+        canOpenDoor = false;
+    }
+    
 }
---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorController : MonoBehaviour {
+    public bool isLocked;
+    public string unlockKeyId;
+    public string sceneDestination;
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        PlayerController controller = other.GetComponent<PlayerController>();
+        if (controller != null) {
+            controller.AvailableDoor(this);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        PlayerController controller = other.GetComponent<PlayerController>();
+        if (controller != null) {
+            controller.NoAvailableDoor();
+        }
+    }
+
+    public void OpenDoor() {
+        SceneManager.LoadScene(sceneDestination);
+    }
+}
