c400e9ac707fe84ee064ade7cb84a662bd16312d
Quim Camprubi Casas <quimcc08@gmail.com>
2022-04-27 18:35:05 +0200

    Gwendoline script

--- Assets/Scripts/Enemies/Gwendoline.cs
+++ Assets/Scripts/Enemies/Gwendoline.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
-public class Gwendoline : MonoBehaviour
-{
+public class Gwendoline : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D _rigidbody2D;
     // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate() 
     {
+        Vector2 position = _rigidbody2D.position;
+        position.x = position.x + Time.deltaTime * speed;
         
+        _rigidbody2D.MovePosition(position);
     }
 }
