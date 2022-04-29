66a7a52f09a85ad4f3069ee4ecb7fd05dbd1bc26
Ferran <ferranmreyes@gmail.com>
2022-04-29 14:46:26 +0200

    added falling platforms + kitchen finished + other details, level almost completed

--- Assets/Scripts/FallingPlatforms/FallingPlatforms.cs
+++ Assets/Scripts/FallingPlatforms/FallingPlatforms.cs
@@ -5,10 +5,15 @@ public class FallingPlatforms : MonoBehaviour
     private Rigidbody2D _rb;
     public float fallingTime;
     public float destroyTime;
+    public AudioClip fallingPlatformSound;
+    private AudioSource _audioSource;
+
     
     // Start is called before the first frame update
     void Start() {
         _rb = GetComponent<Rigidbody2D> ();
+        _audioSource = GetComponent<AudioSource>();
+
     }
 
     void OnCollisionEnter2D (Collision2D col) { 
@@ -20,5 +25,6 @@ public class FallingPlatforms : MonoBehaviour
 
     void DropPlatform() {
         _rb.isKinematic = false;
+        _audioSource.PlayOneShot(fallingPlatformSound);
     }
 }
